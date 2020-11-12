a380ebab6c5afc67e470c78e4048ed820e09142b
Kunal Patel <61086559+Sentinal920@users.noreply.github.com>
2020-11-12 19:29:31 +0530

    Create Progress_bar.sh

---
+++ Progress_bar.sh
@@ -0,0 +1,14 @@
+# Adding Progress bar in a script
+
+# Some Task Here
+echo -ne '...                     (33%)\r'
+sleep 1
+
+# Some Task Here
+echo -ne '......                  (66%)\r'
+sleep 1
+
+# Some Task Here
+echo -ne '..........              (100%)\r'
+
+echo -ne '\n'
