1c95001c24bd21066cb13544158c3ff602b220ba
Kunal Patel <61086559+Sentinal920@users.noreply.github.com>
2021-12-17 09:48:34 +0530

    Create Add_inbound_firewall.sh

---
+++ Add_inbound_firewall.sh
@@ -0,0 +1,12 @@
+# Performs a check to ensure that only ports 22 and 80 are listening externally
+echo "[+] Check open ports"
+echo "Externally running ports"
+ss -tl | awk '$4~/0.0.0.0/ {print $4}'
+
+sudo ufw default deny incoming &> /dev/null
+sudo ufw default allow outgoing &> /dev/null
+sudo ufw allow ssh &> /dev/null
+sudo ufw allow 80/tcp &> /dev/null
+sudo ufw enable &> /dev/null
+echo -e '\n'
+echo 'Added Firewall Rule to only allow port 22 and 80'
