2044effec820b1007fb8e99c40f4a930e5ddf3d2
Kunal Patel <61086559+Sentinal920@users.noreply.github.com>
2021-12-17 09:48:49 +0530

    Create Disable_ipv6.sh

---
+++ Disable_ipv6.sh
@@ -0,0 +1,7 @@
+# Disable IPv6 
+echo "[+] Disable ipv6"
+echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=""/GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1"/' /etc/default/grub
+sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="ipv6.disable=1"/' /etc/default/grub
+update-grub
