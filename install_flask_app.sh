fe26d8d1675608a32ec49102bdb0e0ed3a191a38
Kunal Patel <61086559+Sentinal920@users.noreply.github.com>
2022-01-10 11:26:07 +0530

    Create install_flask_app.sh

---
+++ install_flask_app.sh
@@ -0,0 +1,77 @@
+echo "[+] Installing utilities"
+apt update -y
+
+echo "[+] Installing Dependencies"
+apt install -y curl zip unzip 
+apt install python3-pip -y
+apt install nginx python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools checkinstall -y
+apt install python3-venv -y
+pip3 install pyinstaller
+pip3 install flask
+
+
+echo "[+] Adding Website"
+mkdir /var/www/app
+unzip -o app.zip -d /var/www/app
+cd /var/www/app/ && chmod +x app.py && chmod +x wsgi.py
+
+python3 -m venv pixel
+source pixel/bin/activate
+pip3 install wheel
+pip3 install uwsgi flask
+deactivate
+
+
+cat <<EOF > /var/www/app/app.ini
+[uwsgi]
+module = wsgi:app
+
+master = true
+processes = 5
+
+socket = app.sock
+chmod-socket = 660
+vacuum = true
+
+die-on-term = true
+EOF
+
+
+
+cat <<EOF > /etc/systemd/system/app.service
+[Unit]
+Description=uWSGI instance to serve myproject
+After=network.target
+
+[Service]
+User=app
+Group=www-data
+WorkingDirectory=/var/www/app
+ExecStart=/var/www/app/app/bin/uwsgi --ini app.ini
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+cat <<EOF > /etc/nginx/sites-available/app
+server {
+    listen 8080;
+    server_name localhost;
+	 location / {
+		include uwsgi_params;
+        uwsgi_pass unix:/var/www/app/app.sock;
+    }
+}
+EOF
+
+chown -R app:www-data /var/www/app
+cd /var/www/app && chown app:www-data *
+
+
+ln -s /etc/nginx/sites-available/app /etc/nginx/sites-enabled
+systemctl restart nginx
+
+systemctl daemon-reload
+systemctl start app
+systemctl enable app
